c2e2b24d67ecbdc767ad4e758cfaa8491126ddaa
saipranay47 <saipranayadepu07@gmail.com>
2023-08-22 14:19:54 +0530

    Tooltip component
    -  [x] Create a Tooltip component that takes in nessaary props and renders a tooltip
    -  [x] Add the Tooltip component to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
+import React from "react";
+import Tooltip from "./components/Tooltip";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  height: 100%;
+  width: 100vw;
+`;
+
+const Button = styled.button`
+  width: fit-content;
+  padding: 10px;
+  border-radius: 4px;
+  border: none;
+  background-color: #f9f9f9;
+  color: #000;
+  cursor: pointer;
+`;
+
+
 function App() {
   return (
-    <>
-      <div className="app">
-        <h1>tooltips</h1>
-      </div>
-    </>
+    <Container>
+      <Tooltip
+        text={"This is a tooltip"}
+        bgcolor="black"
+        textcolor="white"
+        padding="10px"
+        cornerradius="4px"
+        tooltipwidth="250px"
+        arrowheight="10px"
+        horizontalposition={"center"}
+        verticalposition={"bottom"}
+      >
+        <Button data-tooltip="Hover me for a tooltip">Hover me</Button>
+      </Tooltip>
+    </Container>
   );
 }
+
 
 export default App;
---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import styled from "styled-components";
+
+const TooltipWrapper = styled.div`
+  position: relative;
+  display: inline-flex;
+  flex-direction: ${(props) =>
+    props.verticalposition === "top" ? "column-reverse" : "column"};
+  justify-content: center;
+  align-items: ${(props) => props.horizontalposition || "center"};
+`;
+
+const TooltipContent = styled.div`
+  position: relative;
+  text-align: center;
+  background-color: ${(props) => props.bgcolor || "lightgray"};
+  color: ${(props) => props.textcolor || "black"};
+  padding: ${(props) => props.padding || "8px"};
+  border-radius: ${(props) => props.cornerradius || "4px"};
+  width: ${(props) => props.tooltipwidth || "max-content"};
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  margin-top: ${(props) =>
+    props.verticalposition === "top" ? "0" : props.arrowheight || "8px"};
+  margin-bottom: ${(props) =>
+    props.verticalposition === "bottom" ? "0" : props.arrowheight || "8px"};
+
+  &::before {
+    content: "";
+    position: absolute;
+    border-style: solid;
+    border-color: transparent;
+    border-width: ${(props) => props.arrowheight || "8px"};
+    border-bottom-color: ${(props) => props.bgcolor || "lightgray"};
+    left: ${(props) =>
+      props.horizontalposition === "start"
+        ? "20px"
+        : props.horizontalposition === "end"
+        ? "auto"
+        : "50%"};
+    right: ${(props) => (props.horizontalposition === "end" ? "20px" : "auto")};
+    bottom: 100%;
+    top: ${(props) => (props.verticalposition === "top" ? "100%" : "unset")};
+
+    transform: ${(props) =>
+      props.horizontalposition === "center"
+        ? "translateX(-50%)" +
+          (props.verticalposition === "top" ? " rotate(180deg)" : "")
+        : props.verticalposition === "top"
+        ? "rotate(180deg)"
+        : "none"};
+  }
+`;
+
+const Tooltip = ({
+  text,
+  children,
+  bgcolor,
+  textcolor,
+  padding,
+  cornerradius,
+  tooltipwidth,
+  arrowheight,
+  horizontalposition,
+  verticalposition,
+}) => {
+  return (
+    <TooltipWrapper
+      horizontalposition={horizontalposition}
+      verticalposition={verticalposition}
+    >
+      {children}
+
+      <TooltipContent
+        bgcolor={bgcolor}
+        textcolor={textcolor}
+        padding={padding}
+        cornerradius={cornerradius}
+        tooltipwidth={tooltipwidth}
+        arrowheight={arrowheight}
+        horizontalposition={horizontalposition}
+        verticalposition={verticalposition}
+      >
+        {text}
+      </TooltipContent>
+    </TooltipWrapper>
+  );
+};
+
+Tooltip.propTypes = {
+  text: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
+  padding: PropTypes.string,
+  cornerRadius: PropTypes.string,
+  tooltipWidth: PropTypes.string,
+  arrowHeight: PropTypes.string,
+  horizontalPosition: PropTypes.oneOf(["start", "center", "end"]),
+  verticalPosition: PropTypes.oneOf(["top", "bottom"]),
+};
+
+export default Tooltip;
